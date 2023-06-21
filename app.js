4f008d597f403a04f3880e6aee9443493cd8b496
gm017 <114661394+gm017@users.noreply.github.com>
2023-06-21 11:45:54 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 let express = require('express');
 
+
 let app = express();
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 
 const PORT = process.env.PORT;
 
